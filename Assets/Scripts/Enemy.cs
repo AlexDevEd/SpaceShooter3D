f82ccd37f29ecea7997ac15bb7df7cd7e13b078a
AlexDevEd <alekseik@xcritical.software>
2021-06-01 17:23:52 +0300

    modify enemies, added field of points

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject _deathExplosion;
     [SerializeField] private Transform _parent;
+    [SerializeField] private int _scoreToAdd = 5;
+
+    private Score _scoreBoard;
+   
+    void Start()
+    {
+        _scoreBoard = FindObjectOfType<Score>();
+    }
 
     void OnParticleCollision(GameObject other)
     {
+        _scoreBoard.ScoreHit(_scoreToAdd);
         GameObject explposion = Instantiate(_deathExplosion, transform.position, Quaternion.identity);
         explposion.transform.parent = _parent;
         Destroy(gameObject);
     }
+
 }
